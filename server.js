60ca6d0282e89422652b81acc845c5e8281f8a04
Erick Valencia <evalencia0911@gmail.com>
2023-12-15 11:00:20 -0700

    completed: take on down and pass it around

--- server.js
+++ server.js
@@ -9,7 +9,17 @@ const app = express()
 //route
 
 app.get("/", (req, res) => {
-    res.send("<h1>99 bottles of beer on the wall</h1>")
+    res.send('<h1>99 bottles of beer on the wall</h1><h2><a href="/98">Take one down, pass it around</a></h2>')
+})
+
+app.get("/:numberofbottles", (req, res) => {
+    const totalBottles = req.params.numberofbottles
+        if(totalBottles > 0) {
+            res.send(`<h1>${totalBottles} bottles of beer on the wall</h1><h2><a href="/${(totalBottles) - 1}">Take one down, pass it around</a></h2>`)
+        }else {
+            res.send(`<h1>0 bottles left!</h1><h2><a href="/">Want to start over?</a></h2>`)
+        }
+
 })
 
 //turn on server listener
